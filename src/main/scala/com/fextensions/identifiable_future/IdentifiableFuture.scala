b06d04a39efce73c3e8b0a17b4b2509dbdf5b03a
pamu <p_nagarjuna@students.iitmandi.ac.in>
2017-01-04 21:25:16 +0530

    initial identifiable future

--- src/main/scala/com/fextensions/identifiable_future/IdentifiableFuture.scala
+++ src/main/scala/com/fextensions/identifiable_future/IdentifiableFuture.scala
@@ -1,14 +1,20 @@
 package com.fextensions.identifiable_future
 
-import scala.concurrent.Future
+import scala.concurrent.{Future, Promise}
+import scala.util.{Success, Try}
 
+sealed trait IdentifiableFuture[I, +A] {
+  self =>
 
-sealed trait IdentifiableFuture[I, +A] { self =>
+  type pair = (I, A)
+
   def id: I
+
   def future: Future[A]
 
   def map[B](f: A => B): IdentifiableFuture[I, B] = new IdentifiableFuture[I, B] {
     override def id: I = self.id
+
     override def future: Future[B] = self.future.map(f)
   }
 
@@ -19,21 +25,47 @@ sealed trait IdentifiableFuture[I, +A] { self =>
 
   def recover[B >: A](f: PartialFunction[Throwable, B]) = new IdentifiableFuture[I, B] {
     override def id: I = self.id
+
     override def future: Future[B] = self.future.recover(f)
   }
 
   def recoverWith[B >: A](f: PartialFunction[Throwable, IdentifiableFuture[I, B]]) = new IdentifiableFuture[I, B] {
     override def id: I = self.id
+
     override def future: Future[B] = self.future.recoverWith { case th => f(th).future }
+  }
+
+  def tryMap[B](f: Try[A] => B): IdentifiableFuture[I, B] = {
+    val promise = Promise[B]()
+    self.future.onComplete(value => promise.tryComplete(Success(f(value))))
+    new IdentifiableFuture[I, B] {
+      override def id: I = self.id
+      override def future: Future[B] = promise.future
+    }
+  }
+
+  def tryFlatMap[B](f: Try[A] => IdentifiableFuture[I, B]): IdentifiableFuture[I, B] = {
+    val promise = Promise[B]()
+    self.future.onComplete(value => promise.tryCompleteWith(f(value).future))
+    new IdentifiableFuture[I, B] {
+      override def id: I = self.id
+      override def future: Future[B] = promise.future
+    }
   }
 }
 
-case class FutureWithId[+I, +A](override val id: I, override val future: Future[A]) extends IdentifiableFuture[I, A]
+case class FutureWithId[+I, +A](override val id: I)(override val future: Future[A]) extends IdentifiableFuture[I, A]
 
 object IdentifiableFuture {
+  
+  def apply[I, A](id: I, future: Future[A]): IdentifiableFuture[I, A] = new IdentifiableFuture[I, A]() {
+    override def id: I = id
+    override def future: Future[A] = future
+  }
 
   def successful[I, A](id: I, value: A) = new IdentifiableFuture[I, A] {
     override def id: I = id
+
     override def future: Future[A] = Future.successful(value)
   }
 
